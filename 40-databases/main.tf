fc242d7a7f491ca150c1a2ef82827495c8c4bd45
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-11-02 12:21:11 +0530

    my commits

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -40,129 +40,129 @@ resource "terraform_data" "mongodb" {
 }
 
 
-# # Terraform code to create redis ec2 instance
-# resource "aws_instance" "redis" {
-#     ami = local.ami_id
-#     instance_type = "t3.micro"
-#     vpc_security_group_ids = [local.redis_sg_id]
-#     subnet_id = local.database_subnet_id
+# Terraform code to create redis ec2 instance
+resource "aws_instance" "redis" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.redis_sg_id]
+    subnet_id = local.database_subnet_id
     
-#     tags = merge (
-#         local.common_tags,
-#         {
-#             Name = "${local.common_name_suffix}-redis" # roboshop-dev-redis
-#         }
-#     )
-# }
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-redis" # roboshop-dev-redis
+        }
+    )
+}
 
-# resource "terraform_data" "redis" {
-#   triggers_replace = [ #If ec2 instance id is changed then terraform_data block starts its execution
-#     aws_instance.redis.id
-#   ]
+resource "terraform_data" "redis" {
+  triggers_replace = [ #If ec2 instance id is changed then terraform_data block starts its execution
+    aws_instance.redis.id
+  ]
   
-#   connection {
-#     type     = "ssh"
-#     user     = "ec2-user"
-#     password = "DevOps321"
-#     host     = aws_instance.redis.private_ip
-#   }
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.redis.private_ip
+  }
 
-# # terraform copies the file to mongodb server
-#     provisioner "file" {
-#     source = "bootstrap.sh"
-#     destination = "/tmp/bootstarp.sh" 
-#   }
+# terraform copies the file to mongodb server
+    provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstarp.sh" 
+  }
 
-#   provisioner "remote-exec" {
-#     inline = [
-#       "chmod +x /tmp/bootstarp.sh",
-#       "sudo sh /tmp/bootstarp.sh redis"
-#     ]
-#   }
-# }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstarp.sh",
+      "sudo sh /tmp/bootstarp.sh redis"
+    ]
+  }
+}
 
 
 
-# # Terraform code to create rabbitmq ec2 instance
-# resource "aws_instance" "rabbitmq" {
-#     ami = local.ami_id
-#     instance_type = "t3.micro"
-#     vpc_security_group_ids = [local.rabbitmq_sg_id]
-#     subnet_id = local.database_subnet_id
+# Terraform code to create rabbitmq ec2 instance
+resource "aws_instance" "rabbitmq" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.rabbitmq_sg_id]
+    subnet_id = local.database_subnet_id
     
-#     tags = merge (
-#         local.common_tags,
-#         {
-#             Name = "${local.common_name_suffix}-rabbitmq" # roboshop-dev-redis
-#         }
-#     )
-# }
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-rabbitmq" # roboshop-dev-redis
+        }
+    )
+}
 
-# resource "terraform_data" "rabbitmq" {
-#   triggers_replace = [ #If ec2 instance id is changed then terraform_data block starts its execution
-#     aws_instance.rabbitmq.id
-#   ]
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [ #If ec2 instance id is changed then terraform_data block starts its execution
+    aws_instance.rabbitmq.id
+  ]
   
-#   connection {
-#     type     = "ssh"
-#     user     = "ec2-user"
-#     password = "DevOps321"
-#     host     = aws_instance.rabbitmq.private_ip
-#   }
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.rabbitmq.private_ip
+  }
 
-# # terraform copies the file to rabbitmq server
-#     provisioner "file" {
-#     source = "bootstrap.sh"
-#     destination = "/tmp/bootstarp.sh" 
-#   }
+# Terraform copies the file to rabbitmq server
+    provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstarp.sh" 
+  }
 
-#   provisioner "remote-exec" {
-#     inline = [
-#       "chmod +x /tmp/bootstarp.sh",
-#       "sudo sh /tmp/bootstarp.sh rabbitmq"
-#     ]
-#   }
-# }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstarp.sh",
+      "sudo sh /tmp/bootstarp.sh rabbitmq"
+    ]
+  }
+}
 
 
 
-# # Terraform code to create mysql ec2 instance
-# resource "aws_instance" "mysql" {
-#     ami = local.ami_id
-#     instance_type = "t3.micro"
-#     vpc_security_group_ids = [local.mysql_sg_id]
-#     subnet_id = local.database_subnet_id
+# Terraform code to create mysql ec2 instance
+resource "aws_instance" "mysql" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.mysql_sg_id]
+    subnet_id = local.database_subnet_id
     
-#     tags = merge (
-#         local.common_tags,
-#         {
-#             Name = "${local.common_name_suffix}-mysql" # roboshop-dev-mysql
-#         }
-#     )
-# }
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-mysql" # roboshop-dev-mysql
+        }
+    )
+}
 
-# resource "terraform_data" "mysql" {
-#   triggers_replace = [ #If ec2 instance id is changed then terraform_data block starts its execution
-#     aws_instance.mysql.id
-#   ]
+resource "terraform_data" "mysql" {
+  triggers_replace = [ #If ec2 instance id is changed then terraform_data block starts its execution
+    aws_instance.mysql.id
+  ]
   
-#   connection {
-#     type     = "ssh"
-#     user     = "ec2-user"
-#     password = "DevOps321"
-#     host     = aws_instance.mysql.private_ip
-#   }
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mysql.private_ip
+  }
 
-# # terraform copies the file to mysql server
-#     provisioner "file" {
-#     source = "bootstrap.sh"
-#     destination = "/tmp/bootstarp.sh" 
-#   }
+# Terraform copies the file to mysql server
+    provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstarp.sh" 
+  }
 
-#   provisioner "remote-exec" {
-#     inline = [
-#       "chmod +x /tmp/bootstarp.sh",
-#       "sudo sh /tmp/bootstarp.sh mysql"
-#     ]
-#   }
-# }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstarp.sh",
+      "sudo sh /tmp/bootstarp.sh mysql"
+    ]
+  }
+}
