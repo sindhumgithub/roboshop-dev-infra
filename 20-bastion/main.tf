ece1482964fd95d0cc1e2897c31be4773dbfa60b
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-11-04 15:08:44 +0530

    my commits

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "bastion" {
   instance_type = var.instance_type
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id = local.Public_subnet_id
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
 
   tags = merge (
     local.common_tags,
@@ -11,3 +12,9 @@ resource "aws_instance" "bastion" {
     }
   )
   }
+
+# code to read SSM parameter store parameters using  IAM role 
+resource "aws_iam_instance_profile" "bastion" {
+  name = "bastion"
+  role = "BastionTerraformAdmin"
+}
