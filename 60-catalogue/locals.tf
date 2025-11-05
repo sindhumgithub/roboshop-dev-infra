7b3ef6a532303467c8eae6c3d83ab3f965a6f64a
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-11-05 16:17:32 +0530

    my commits

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -5,7 +5,7 @@ locals {
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     ami_id = data.aws_ami.joindevops.id
-    backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener.value
+    backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
 
     common_tags = {
         Project = var.project_name
