03814e4a2636d7b7593265eeed011e6e244332e1
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-11-05 13:48:10 +0530

    my commits

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,7 +1,9 @@
 locals {
     common_name_suffix = "${var.project_name}-${var.environment}" #roboshop-dev
     private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0] 
+    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
     ami_id = data.aws_ami.joindevops.id
 
     common_tags = {
