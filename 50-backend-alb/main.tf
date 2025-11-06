5fd2b1a5bb0eca575178428dcdc59e751a171581
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-11-06 14:39:46 +0530

    my commits

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -35,7 +35,7 @@ resource "aws_lb_listener" "backend_alb" {
 
 resource "aws_route53_record" "backend_alb" {
   zone_id = var.zone_id
-  name    = "*.backend-alb.${var.environment}.${var.domain_name}"
+  name    = "*.backend-alb-${var.environment}.${var.domain_name}"
   type    = "A"
 # These are Backend application load balancer details, not domain details.
   alias {
