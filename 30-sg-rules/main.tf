08eb5ce359ab0612a30c2bd91a6615651c4c672a
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-11-06 13:22:40 +0530

    my commits

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -80,3 +80,13 @@ resource "aws_security_group_rule" "catalogue_mongodb" {
   protocol          = "tcp"
   to_port           = 27017
 }
+
+# connection from backendalb to catalogue
+resource "aws_security_group_rule" "catalogue_backend_alb" {
+  type              = "ingress"
+  security_group_id = local.catalogue_sg_id
+  source_security_group_id = local.backend_alb_sg_id
+  from_port         = 8080
+  protocol          = "tcp"
+  to_port           = 8080
+}
