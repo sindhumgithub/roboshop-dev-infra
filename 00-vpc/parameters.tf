7fbbb036e7e3ac36427f385b313aa673ab3f03c6
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-11-09 14:59:07 +0530

    my commits

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -6,19 +6,19 @@ resource "aws_ssm_parameter" "vpc_id" {
 
 
 resource "aws_ssm_parameter" "public_subnet_ids" {
-  name  = "/${var.project_name}/${var.environment}/public_subnet_id"
+  name  = "/${var.project_name}/${var.environment}/public_subnet_ids"
   type  = "StringList"
   value = join("," , module.vpc.public_subnet_ids) # converts list to string
 }
 
 resource "aws_ssm_parameter" "private_subnet_ids" {
-  name  = "/${var.project_name}/${var.environment}/private_subnet_id"
+  name  = "/${var.project_name}/${var.environment}/private_subnet_ids"
   type  = "StringList"
   value = join("," , module.vpc.private_subnet_ids)
 }
 
 resource "aws_ssm_parameter" "database_subnet_ids" {
-  name  = "/${var.project_name}/${var.environment}/database_subnet_id"
+  name  = "/${var.project_name}/${var.environment}/database_subnet_ids"
   type  = "StringList"
   value = join("," , module.vpc.database_subnet_ids)
 }
