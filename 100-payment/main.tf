29bc965426ee68921baf2eb706eed24c2d9c35fb
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-11-10 09:08:12 +0530

    my commits

---
+++ 100-payment/main.tf
@@ -0,0 +1,11 @@
+module "shipping" {
+  source = "git::https://github.com/sindhumgithub/terraform-aws-services.git?ref=master"
+  project_name   = var.project_name
+  environment    = var.environment
+  instance_type = var.instance_type
+  service_name   = var.service_name
+  ssh_user         = "ec2-user"
+  ssh_password     = "DevOps321"
+  priority = 50
+  domain_name = var.domain_name
+}
---
+++ 100-payment/variables.tf
@@ -0,0 +1,25 @@
+variable "project_name" {
+  type        = string
+  default = "roboshop"
+}
+
+variable "environment" {
+  type        = string
+  default =  "dev"
+}
+
+variable "instance_type" {
+  type        = string
+  default = "t3.micro"
+}
+
+variable "service_name" {
+  type = string
+  default = "payment"
+}
+
+variable "domain_name" {
+  type = string
+  default = "sindhuworld.icu"
+}
+
