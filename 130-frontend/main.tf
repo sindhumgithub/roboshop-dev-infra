696807a8a1b8669f64829f5c0dcd30c38cbf7de0
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-11-11 14:08:53 +0530

    commits

--- 130-frontend/main.tf
+++ 130-frontend/main.tf
@@ -74,14 +74,14 @@ resource "aws_lb_target_group" "frontend" {
   vpc_id   = local.vpc_id
   deregistration_delay = 60  #waiting period before deleting the instance.
   health_check {
-    healthy_threshold = 2
-    interval = 10
+    healthy_threshold = 5
+    interval = 300
     matcher = "200-299"
     path = "/"
     port = 80
     protocol = "HTTP"
-    timeout = 2
-    unhealthy_threshold = 2
+    timeout = 5
+    unhealthy_threshold = 5
   }
 }
 
