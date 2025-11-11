ec58051f9b1d3db7713d99b38b55eea5f52a6498
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-11-11 08:50:01 +0530

    commits

--- 130-frontend/data.tf
+++ 130-frontend/data.tf
@@ -26,6 +26,10 @@ data "aws_ssm_parameter" "user_sg_id" {
   name = "/${var.project_name}/${var.environment}/user_sg_id"
 }
 
+data "aws_ssm_parameter" "frontend_sg_id" {
+  name = "/${var.project_name}/${var.environment}/frontend_sg_id"
+}
+
 data "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project_name}/${var.environment}/vpc_id"
 }
