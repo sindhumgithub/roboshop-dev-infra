987ada7dc60ba602edda4e57e5ed5f3c1e6718c8
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-11-11 09:30:30 +0530

    commits

--- 120-frontend-alb/parameters.tf
+++ 120-frontend-alb/parameters.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "frontend_alb_listener" {
-  name  = "/${var.project_name}/${var.environment}/frontend_alb_listener"
+resource "aws_ssm_parameter" "frontend_alb_listener_arn" {
+  name  = "/${var.project_name}/${var.environment}/frontend_alb_listener_arn"
   type  = "String"
   value = aws_acm_certificate.roboshop.arn
 }
