aa8ebac10062f28d1fcd145007120847d2fc1dee
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-11-11 09:39:35 +0530

    commits

--- 120-frontend-alb/locals.tf
+++ 120-frontend-alb/locals.tf
@@ -4,8 +4,7 @@ locals {
     frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
     public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
     frontend_alb_certificate_arn = data.aws_ssm_parameter.frontend_alb_certificate_arn.value
-    frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
-
+    
     common_tags = {
         project = var.project_name
         Environment = var.environment
