526fdbba7f14c1d0511e0f667f82f37965bbac53
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-11-11 09:49:59 +0530

    commits

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -272,3 +272,12 @@ resource "aws_security_group_rule" "frontend_frontend_alb" {
   protocol          = "tcp"
   to_port           = 80
 }
+
+resource "aws_security_group_rule" "frontend_bastion" {
+  type              = "ingress"
+  security_group_id = local.frontend_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
